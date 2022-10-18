7e7c156ae7c8d2ef33f0e421b14d6133efaa5e66
Patryk Murzyn <patrykmurzyn@pm.me>
2022-10-18 18:55:20 +0200

    update

--- main.py
+++ main.py
@@ -24,18 +24,6 @@ menu = {
     3: 'Exit',
 }
 
-def mainOptions(choice):
-    print('Choice: ', choice)
-    match choice:
-        case 1:
-            print('Wybrano 1')
-        case 2:
-            print('Wybrano 2')
-        case 3:
-            exit()
-        case _:
-            print('Inne')
-
 mainPage = Page(title, menu)
 
 title = 'Leaderboards:'
@@ -57,13 +45,42 @@ menu = {
 
 leaderboardsPage = Page(title, menu)
 
+def mainOptions(choice):
+    match choice:
+        case 1:
+            print('Wybrano 1')
+        case 2:
+            leaderboardsPage.print_menu()
+            response = requests.get("https://api.chess.com/pub/leaderboards").json()
+            choice = int(input('Choice: '))
+            os.system('cls')
+            leaderboardsOptions(choice, response)
+            
+
+        case 3:
+            exit()
+        case _:
+            print('Inne')
+
+def leaderboardsOptions(choice, response):
+    if choice >=3 and choice <=10:
+        for i in range(50):
+            print(i+1, '-', response['live_' + (leaderboardsPage.menu[choice]).lower()][i]['username'], '-', response['live_' + (leaderboardsPage.menu[choice]).lower()][i]['score'], '-', response['live_' + (leaderboardsPage.menu[choice].lower())][i]['url'])
+    else:
+        for i in range(50):
+            print(i+1, '-', response[(leaderboardsPage.menu[choice]).lower()][i]['username'], '-', response[(leaderboardsPage.menu[choice]).lower()][i]['score'], '-', response[(leaderboardsPage.menu[choice].lower())][i]['url'])
+    
+    
+
+
+#---------------------------------------------
+
 mainPage.print_menu()
-choice = int(input('Choice: '))
 
 match Page.activePage:
     #mainPage
     case 0:
-        print('Wybrano 0')
+        choice = int(input('Choice: '))
         mainOptions(choice)
     #leaderboardsPage
     case 1:
